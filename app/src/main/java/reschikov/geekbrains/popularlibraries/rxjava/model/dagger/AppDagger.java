b3944dc39209d640216a35a479360b66a5ac5e1d
Maxim <Reschikov@list.ru>
2019-05-20 15:15:31 +0800

    Выставить настройки ProGuard: minifyEnabled, multiDexEnabled, shrinkResources
    Создать искусственную утечку и отловить её с помощью LeakCanary

--- app/src/main/java/reschikov/geekbrains/popularlibraries/rxjava/model/dagger/AppDagger.java
+++ app/src/main/java/reschikov/geekbrains/popularlibraries/rxjava/model/dagger/AppDagger.java
@@ -2,18 +2,33 @@ package reschikov.geekbrains.popularlibraries.rxjava.model.dagger;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
+
 public class AppDagger extends Application {
 
-	private static GreenComponent greenComponent;
+	private static AppDagger instance;
+	private GreenComponent greenComponent;
+	private RefWatcher watcher;
+
+	public static AppDagger getInstance() {
+		return instance;
+	}
+
+	public GreenComponent getGreenComponent() {
+		return greenComponent;
+	}
+
+	public RefWatcher getWatcher() {
+		return watcher;
+	}
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		if (!LeakCanary.isInAnalyzerProcess(this)) watcher = LeakCanary.install(this);
+		instance = this;
 		greenComponent = generateAppComponent();
-	}
-
-	public static GreenComponent getGreenComponent() {
-		return greenComponent;
 	}
 
 	private GreenComponent generateAppComponent(){
